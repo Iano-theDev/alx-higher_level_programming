c8bad6a981dbb71064bcebed0b0cc562f2e7b68c
Iano-theDev <sinceianmike@gmail.com>
2023-11-01 11:22:05 +0300

    Convert all lowercase characters to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    i = 0
+    while i < len(str):
+        char = ord(str[i])
+        if ord('a') <= char <= ord('z'):
+            char -= 32
+        print("{:c}".format(char), end='')
+        i += 1
