a6da49b674cc3ffcbcf7e140f70b5da2830d65d6
Iano-theDev <sinceianmike@gmail.com>
2023-11-01 17:55:59 +0300

    Fix: check for a null pointer when looping, in case of addding at the end

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -28,7 +28,7 @@ listint_t *insert_node(listint_t **head, int number)
 		return (*head);
 	}
 
-	while (current->n < number && current->next->n < number)
+	while (current->next != NULL && current->next->n < number)
 		current = current->next;
 	new->next = current->next;
 	current->next = new;
