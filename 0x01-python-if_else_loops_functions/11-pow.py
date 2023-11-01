dd648b9595f016bbeb4a812578811b05ab1b3d5b
Iano-theDev <sinceianmike@gmail.com>
2023-11-01 12:51:02 +0300

    Refactor: use the python ** opertor

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    power = 1
-    if b < 0:
-        for i in range(-b):
-            power /= a
-        return power
-    else:
-        for i in range(b):
-            power *= a
-        return power
+    return a**b
