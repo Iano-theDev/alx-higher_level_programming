4a4806fe906c2fc3fc81023e4b7ab17c11fe74fe
Iano-theDev <sinceianmike@gmail.com>
2023-11-03 09:07:41 +0300

    Print all names included in a locally compiled pyython module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4 as h
+    names = dir(h)
+    for name in names:
+        if name[:2] == '__':
+            continue
+        print(name)
