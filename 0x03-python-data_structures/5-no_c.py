16843db868e687f4c53c3f1d5c3d020697aa90e1
Iano-theDev <sinceianmike@gmail.com>
2023-11-08 20:03:53 +0300

    Refactor, use slice operator to perform a shallow copy

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,6 @@
 def no_c(my_string):
     new_list = ''
     for c in my_string:
-        if c == 'c' or c == 'C':
-            continue
-        else:
+        if c != 'c' or c != 'C':
             new_list += c
     return new_list
