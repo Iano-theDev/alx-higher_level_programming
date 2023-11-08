858d8660acf1e12c19a345cedcf3a8124338b71f
Iano-theDev <sinceianmike@gmail.com>
2023-11-08 19:17:29 +0300

    Add checks if index is out of range

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     list_cpy = my_list
-    if my_list:
+    if my_list and 0 <= idx < len(my_list):
         list_cpy[idx] = element
     return list_cpy
