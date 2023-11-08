de6fa795ddae6a94d6dc0689f4b20c8345135018
Iano-theDev <sinceianmike@gmail.com>
2023-11-08 19:12:07 +0300

    Refactor

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    list_cpy = my_list
     if my_list:
-        list_cpy = my_list
         list_cpy[idx] = element
     return list_cpy
