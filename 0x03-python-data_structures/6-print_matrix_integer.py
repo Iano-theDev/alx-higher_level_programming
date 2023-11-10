3bbc49759f5c1a9c5a4b24085538a300210f08bf
Iano-theDev <sinceianmike@gmail.com>
2023-11-10 18:43:58 +0300

    Check for last item in the matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,8 @@ def print_matrix_integer(matrix=[[]]):
         for i in matrix:
             if i:
                 for j in i:
-                    print("{:d} ".format(j), end="")
+                    if j != i[:-1]:
+                        print("{:d} ".format(j), end="")
+                    else:
+                        print("{:d}".format(j), end="")
             print()
