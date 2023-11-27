09798bc0c0498bbc476a0da6c60e6ef5dab711a1
Iano-theDev <sinceianmike@gmail.com>
2023-11-27 11:18:35 +0300

    Fix linter issues

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-        for line in matrix:
-            if not line:
-                print()
-            for num in line:
-                if num is line[-1]:
-                    print(num)
-                else:
-                    print("{:d}, ".format(num), end='')
+    for line in matrix:
+        if not line:
+            print()
+        for num in line:
+            if num is line[-1]:
+                print(num)
+            else:
+                print("{:d}, ".format(num), end='')
