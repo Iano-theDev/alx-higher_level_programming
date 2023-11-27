192ebb78b1037ec1353bcb7c5d96f9fbe229bb4e
Iano-theDev <sinceianmike@gmail.com>
2023-11-27 12:24:37 +0300

    Refactor, using join

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
-        if not line:
-            print()
-        for num in line:
-            if num is line[-1]:
-                print(num)
-            else:
-                print("{:d}, ".format(num), end='')
+        if line:
+            print(", ".join("{:d}".format(num) for num in line), end='')
+        print()
