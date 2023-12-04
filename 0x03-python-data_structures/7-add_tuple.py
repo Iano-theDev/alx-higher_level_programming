2c1797c70abb60c67e98c30676d2b9c85056d929
Iano-theDev <sinceianmike@gmail.com>
2023-12-04 14:24:57 +0300

    Add tuple and return a new tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    el_0 = 0
-    el_1 = 0
-    for i in tuple_a:
-        
+    a_0 = tuple_a[0] if len(tuple_a) >= 1 else 0
+    b_0 = tuple_b[0] if len(tuple_b) >= 1 else 0
+    a_1 = tuple_a[1] if len(tuple_a) >= 2 else 0
+    b_1 = tuple_b[1] if len(tuple_b) >= 2 else 0
+    return (a_0 + b_0, a_1 + b_1)
