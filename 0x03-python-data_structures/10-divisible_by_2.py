0f498bbd3f7d06c5b3e91487e37bb82f825bfd12
Iano-theDev <sinceianmike@gmail.com>
2023-12-04 17:13:13 +0300

    Fix, append true of false to the new list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,6 +2,6 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for n in my_list:
-        x = n % 2
+        x = True if n % 2 == 0 else False
         new_list.append(x)
     return new_list
