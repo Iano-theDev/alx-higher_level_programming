83e3998ae6ec7c3714f13cc3ebcab1fa3de9213c
Iano-theDev <sinceianmike@gmail.com>
2023-12-05 17:02:00 +0300

    Write a function to make a copy of a list before reversing it

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 listint_t *reverse_list(listint_t **head);
+listint_t *copy_list(listint_t **head);
 
 /**
 * is_palindrome - checks if listint_t is a palinrome
@@ -11,7 +13,8 @@ listint_t *reverse_list(listint_t **head);
 */
 int is_palindrome(listint_t **head)
 {
-	listint_t *rev = reverse_list(head), *current = *head;
+	listint_t *head_copy = copy_list(head), *current = *head;
+	listint_t *rev = reverse_list(&head_copy);
 
 	if (*head == NULL)
 		return (0);
@@ -24,8 +27,12 @@ int is_palindrome(listint_t **head)
 			rev = rev->next;
 		}
 		else
+		{
+			free_listint(head_copy);
 			return (0);
+		}
 	}
+	free_listint(head_copy);
 	return (1);
 }
 
@@ -48,4 +55,39 @@ listint_t *reverse_list(listint_t **head)
 	*head = prev;
 
 	return (*head);
+}
+/**
+* copy_list - returns a copy of a listint_t list
+* @head: pointer to the head of a listint_t list
+* Return: pointer to new listint_t list
+*/
+listint_t *copy_list(listint_t **head)
+{
+	listint_t *new, *ptr, *current = *head;
+
+	if (*head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+	ptr = new;
+
+	while (current != NULL)
+	{
+		ptr->n = current->n;
+
+		if (current->next != NULL)
+		{
+			ptr->next = malloc(sizeof(listint_t));
+
+			if (ptr->next == NULL)
+				return (NULL);
+			ptr = ptr->next;
+		}
+		ptr->next = NULL;
+		current = current->next;
+	}
+	return (new);
 }
