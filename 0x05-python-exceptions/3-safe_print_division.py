966833751567630ce0ff04e48db5174986ad36e0
Iano-theDev <sinceianmike@gmail.com>
2023-12-15 07:12:24 +0300

    Fix print statement

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except (ZeroDivisionError, ValueError, TypeError):
         pass
     finally:
-        print("Inside result {}".format(div))
+        print("Inside result: {}".format(div))
         return div
