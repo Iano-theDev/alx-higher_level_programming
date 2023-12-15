a509b490124e5915c093ec1d079f2b458cfdbeff
Iano-theDev <sinceianmike@gmail.com>
2023-12-15 08:43:00 +0300

    Fix typo error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,7 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
         except ZeroDivisionError:
             result = 0
-            print("division by zero")
+            print("division by 0")
         finally:
             new_list.append(result)
     return new_list
