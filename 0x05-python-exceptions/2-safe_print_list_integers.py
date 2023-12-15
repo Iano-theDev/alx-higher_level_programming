65a0cb7f4ce32fabc139bbf359577bfe79a09dfb
Iano-theDev <sinceianmike@gmail.com>
2023-12-15 06:22:47 +0300

    Fix: Use another variable to loop through the list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     idx = 0
-    for idx in range(x):
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[idx]), end='')
+            print("{:d}".format(my_list[i]), end='')
             idx += 1
         except (ValueError, TypeError):
             continue
