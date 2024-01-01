e913e3e92d1efce299ab067d38e07d61b724cba9
Iano-theDev <sinceianmike@gmail.com>
2024-01-01 22:23:57 +0300

    Fix for order of checks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -79,10 +79,10 @@ class Square:
         Return:
             Null"""
         if len(value) != 2\
-           or value[0] < 0\
-           or value[1] < 0\
            or type(value[0]) is not int\
-           or type(value[1]) is not int:
+           or type(value[1]) is not int\
+           or value[0] < 0\
+           or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
