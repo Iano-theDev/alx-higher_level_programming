71a486436727f6f21e2ddd30cdd90deea1d8ef09
Iano-theDev <sinceianmike@gmail.com>
2024-01-01 21:27:34 +0300

    Check case for tuple at both indexes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -78,7 +78,9 @@ class Square:
 
         Return:
             Null"""
-        if len(value) != 2 or value[0] < 0 and value[1] < 0:
+        if len(value) != 2 or value[0] < 0 or value[1] < 0\
+           or type(value[0] is not int\
+           or type(value[1] is not int):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
