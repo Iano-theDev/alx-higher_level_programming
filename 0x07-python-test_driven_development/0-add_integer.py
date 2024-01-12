e217336bf8c5cb3d9054e8053c431ca2b318a7e4
Iano-theDev <sinceianmike@gmail.com>
2024-01-13 00:28:50 +0300

    Create add function and sample test cases

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Addition module
+"""
+
+def add_integer(a, b=98):
+    """
+    Adds two integers and returns the result
+    
+    Parameters:
+    a (int): first integer
+    b (int): second integer
+
+    Return:
+    int: Sum of a and b
+    """
+    if type(a) is not int or type(b) is not float:
+        raise TypeError("a must be an integer")
+    else if type(b) is not int or type(b) is not float:
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,10 @@
+# Python Testing
+# Doctests and Unit Test
+
+* the doctest module is used to test for both doctests and unit tests
+  
+* testing doctest (test files ending in .txt) files is done using the	        testfile() method
+
+* testing modules (test files ending in .py) is done using the
+  testmod() method from the doctest module.
+  
