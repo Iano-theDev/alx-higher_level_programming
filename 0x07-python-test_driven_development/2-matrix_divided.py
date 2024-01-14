bcba00916971c2fc44cb9724ed0d4bde25b987c9
Iano-theDev <sinceianmike@gmail.com>
2024-01-14 22:40:54 +0300

    Minor updates

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Matrix division Module"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Divides each element of the matrix with div
+    
+    Parameters:
+    matrix (list): a list of lists of integers
+    div (int): the element to divide by
+    
+    Return:
+    a list of lists of integers divided with div
+    """
+    
