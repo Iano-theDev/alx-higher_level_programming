9fa5521a78d707c0c9f110b1fddc1e01473821c0
Iano-theDev <sinceianmike@gmail.com>
2024-01-14 10:59:21 +0300

    Fix valueError exception message

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be an integer")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
 
@@ -42,6 +42,6 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be an integer")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
