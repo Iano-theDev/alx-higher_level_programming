1f518e7b321fc8527fd607554ee6e3d8eb2fd24a
Iano-theDev <sinceianmike@gmail.com>
2024-01-16 22:31:06 +0300

    use cls to refer to the class for the class method, add decorators for the class and static methods

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -81,6 +81,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print(f'Bye rectangle...')
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Returns the biggest rectangle based on the area"""
         if not isinstance(rect_1, Rectangle):
@@ -92,6 +93,7 @@ class Rectangle:
         else:
             return rect_1
 
+    @classmethod
     def square(cls, size=0):
         """Returns a new rectangle instance with width == height == size"""
-        return Rectangle(size, size)
+        return cls(size, size)
