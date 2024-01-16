89db61d0b70649033016b3f3b3379afb3324e584
Iano-theDev <sinceianmike@gmail.com>
2024-01-16 08:48:19 +0300

    Add comparison static method to rectangle Class

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
         number_of_instances (int): total instances of class Rectangle
     """
     number_of_instances = 0
-    print_symbol = \#
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         self.height = height
@@ -68,7 +68,8 @@ class Rectangle:
         rect = ""
         if self.width == 0 or self.height == 0:
             return rect
-        rect = ("".join([str(self.print_symbol) for n in range(self.width)]) + "\n") * self.height
+        rect = ("".join([str(self.print_symbol)
+                         for n in range(self.width)]) + "\n") * self.height
         return rect[:-1]
 
     def __repr__(self):
@@ -79,3 +80,14 @@ class Rectangle:
         """Detects deletion of a rectangle"""
         Rectangle.number_of_instances -= 1
         print(f'Bye rectangle...')
+
+    def bigger_or_equal(rect_1, rect_2):
+        """Returns the biggest rectangle based on the area"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        elif rect_2.area() > rect_1.area():
+            return rect_2
+        else:
+            return rect_1
