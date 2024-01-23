af00e578ea8825e4ae4cdcc56a10d6666f4bc99e
Iano-theDev <sinceianmike@gmail.com>
2024-01-23 22:46:19 +0300

    Update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,12 +5,16 @@
 def matrix_divided(matrix, div):
     """
     Divides each element of the matrix with div
-    
+
     Parameters:
     matrix (list): a list of lists of integers
     div (int): the element to divide by
-    
+
     Return:
     a list of lists of integers divided with div
     """
-    
+    new_matrix = []
+    div_list = [div]
+    for n in matrix:
+        new_matrix.append(list(map(lambda a, b: a / b, n, div_list)))
+    return new_matrix
