bf19b6e53aa2982e844fed8897f428f5af506fc1
Iano-theDev <sinceianmike@gmail.com>
2024-02-09 00:54:27 +0300

    Fix divide matrix function and wite tests for matrix_divide module

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,7 +13,21 @@ def matrix_divided(matrix, div):
     Return:
     a list of lists of integers divided with div
     """
-    new_matrix = []
-    for n in matrix:
-        new_matrix.append(list(map(lambda x: round(x / div, 2), n)))
-    return new_matrix
+    size = None
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats");
+    for row in matrix:
+        if size is None:
+            size = len(row)
+        if size != len(row):
+            raise TypeError("Each row of the matrix must have the same size")
+        else:
+            for item in row:
+                if type(item) is not int and type(item) is not float:
+                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    elif div == 0:
+        raise TypeError("division by zero")
+    else:
+        return [[round(x / div, 2) for x in line] for line in matrix]
