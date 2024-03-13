d72908503b04943ba11ee1ba92249285c2b102b0
Iano-theDev <sinceianmike@gmail.com>
2024-03-13 19:48:53 +0300

    Use is operator

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,4 +5,4 @@
 def is_same_class(obj, a_class):
     """Returns true if an object is exactly an instance
        the specified class, otherwise false"""
-    return (type(obj) == a_class)
+    return (type(obj) is a_class)
