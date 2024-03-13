57d57236bb74e18a317d3a6c004cbbebb7e844d2
Iano-theDev <sinceianmike@gmail.com>
2024-03-13 19:59:22 +0300

    Fix function name typo

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 """Is same class module"""
 
 
-def is_same_class(obj, a_class):
-    """Returns true if an object is exactly an instance
-       the specified class, otherwise false"""
+def is_kind_of_class(obj, a_class):
+    """Returns true if an object is an instance
+       the specified class or of a subclass, otherwise false"""
     return isinstance(obj, a_class)
