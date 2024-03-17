4ffab9f0afe05d0f6e7659a6110fff4affc3a099
Iano-theDev <sinceianmike@gmail.com>
2024-03-17 21:59:35 +0300

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write to file module"""
+
+
+def write_file(filename="", text=""):
+    """writes a to a file"""
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
