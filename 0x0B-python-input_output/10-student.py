319d86983008c29cc0679845343745712d9e86d6
Iano-theDev <sinceianmike@gmail.com>
2024-04-03 18:02:39 +0300

    Get specific attribute dictionary representation of a given object instance

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"Student Class Module"
+
+
+class Student:
+    "Represnts a student"
+    def __init__(self, first_name, last_name, age):
+        "Initializes a the sudent class"
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        "retrieves a dictionary representation of a Student instance"
+        attr_dict = {}
+        if attrs:
+            for attr in attrs:
+                try:
+                    attr_dict[attr] = self.__dict__[attr]
+                except Exception:
+                    pass
+            return attr_dict
+        return self.__dict__
+    
