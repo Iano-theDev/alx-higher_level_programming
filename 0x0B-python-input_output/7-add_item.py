030cb18f856ea4a31cecfbb5f7f0a122c5464283
Iano-theDev <sinceianmike@gmail.com>
2024-04-03 07:41:20 +0300

    load arguments from file if file exists

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,9 +3,13 @@
 import sys
 save_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_json = __import__("6-load_from_json_file").load_from_json_file
-argList = []
+filename = "add_item.json"
+
+try:
+    argList = load_json(filename)
+except Exception:
+    argList = []
 
 for arg in sys.argv:
     argList.append(arg)
-save_json_file(argList[1:], "add_item.json")
-load_json("add_item.json")
+save_json_file(argList[1:], filename)
