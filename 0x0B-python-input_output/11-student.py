d977fcd151c141b1972176b91b91b220e6970d79
Iano-theDev <sinceianmike@gmail.com>
2024-04-04 14:24:54 +0300

    Fix for loop to use .items() dictionary method to access key and value for the dictionary

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"Student Class Module"
+"""Student Class Module"""
 
 
 class Student:
-    "Represnts a student"
+    """Represnts a student"""
     def __init__(self, first_name, last_name, age):
-        "Initializes a the sudent class"
+        """Initializes a the sudent class"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        "retrieves a dictionary representation of a Student instance"
+        """retrieves a dictionary representation of a Student instance"""
         if attrs is None:
             return self.__dict__
         attr_dict = {}
@@ -22,9 +22,10 @@ class Student:
                 pass
         return attr_dict
 
-    def reload_from_josn(self, json):
-        "Replaces all attributes of the Student instance using data from the json"
-        for key, value in json:
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance using data
+        from the json"""
+        for key, value in json.items():
             try:
                 self.key = value
             except Exception:
