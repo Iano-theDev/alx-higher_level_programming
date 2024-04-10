398481273dfce346218838bc47b0ce9820955833
Iano-theDev <sinceianmike@gmail.com>
2024-04-10 15:48:32 +0300

    Add rectangle class with getter and setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
 """Rectangle class module"""
-Base = __import__('base').Base
+from models import base
+
+Base = base.Base
+
 
 class Rectangle(Base):
     """Representation of a rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes the rectangle class"""
-        super.__init__(id)
         self.width = width
         self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
 
-    @setter.width
+    @property
+    def width(self):
+        """A getter for the private instance attribute width"""
+        return self.__width
+        
+    @width.setter
     def width(self, value):
         """setter for the private instance attribute width"""
         if type(value) is not int:
@@ -19,11 +29,11 @@ class Rectangle(Base):
             self.__width = value
 
     @property
-    def width(self):
+    def height(self):
         """A getter for the private instance attribute width"""
-        return self.__width
+        return self.__height
 
-    @setter.height
+    @height.setter
     def height(self, value):
         """setter for the private instance attribute height"""
         if type(value) is not int:
@@ -32,7 +42,27 @@ class Rectangle(Base):
             self.__height = value
 
     @property
-    def height:
-        """A getter for the private instance attribute width"""
-        return self.__height
-    
+    def x(self):
+        """A getter for the private instance attribute x(latitude position)"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter for lattitude position of the rectangle"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        else:
+            self.__x = value
+
+    @property
+    def y(self):
+        """A getter for the private instance attribute y (logitude position)"""
+        return self.__y
+            
+    @y.setter
+    def y(self, value):
+        """setter for longitude position of the rectangle"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        else:
+            self.__x = value
