10ecd4ed84996954ac4f712b3a56d67d4ebb77ef
Iano-theDev <sinceianmike@gmail.com>
2024-04-16 06:52:25 +0300

    Fix to json method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,10 +16,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """Returns the JSON string repr of list_dictionaries"""
-        j_list = []
         if list_dictionaries is None:
-            return str(j_list)
-        else:
-            for item in list_dictionaries:
-                j_list.append(json.dumps(item))
-            return str(j_list)
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
