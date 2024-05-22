e94f999a979e1ce5aa2d82dbb85032cb4cc804b4
Iano-theDev <sinceianmike@gmail.com>
2024-05-22 08:33:42 +0300

    Update script, to max 3 lines

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # sends a get request and displays the body of a status 200 response
-response_status=$(curl -sI "$1" | grep "HTTP" | cut -d " " -f 2)
-if [ $response_status -eq "200" ]; then
-    curl -sL "$1"
-fi
+curl -sL "$1"
