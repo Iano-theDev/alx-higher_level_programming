44b62e90ec090ff0df73b0b99edb6fe9d5bd739a
Iano-theDev <sinceianmike@gmail.com>
2024-05-26 17:26:31 +0300

    use curl option -d to pass url parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a post request to a url passed as a variable with given url paramaters
-curl -sX POST "{$1}?email=test@gmail.com&wsubject=I will always be here for PLD"
+curl -sX POST -d "email=test@gmail.com&wsubject=I will always be here for PLD" "$1"
