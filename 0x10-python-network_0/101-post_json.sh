77e3a9318885e37f3855888697e2862f80fdbc85
Iano-theDev <sinceianmike@gmail.com>
2024-05-27 11:07:13 +0300

    switch argument variables

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a json post request to a URL passed as the first argument
-curl -sX POST -H "Content-Type: application/json"  -d "$1" "$2"
+curl -sX POST -H "Content-Type: application/json"  -d "$2" "$1"
