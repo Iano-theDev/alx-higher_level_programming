cfcf4a85d9650cc1c6eb384192f8e6cc33ce537c
Iano-theDev <sinceianmike@gmail.com>
2024-05-27 10:57:15 +0300

    Update script add silent tag and rearrage argument variables

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a json post request to a URL passed as the first argument
-curl -X POST -H "Content-Type: application/json"  -d "$2" "$1"
+curl -sX POST -H "Content-Type: application/json"  -d "$1" "$2"
